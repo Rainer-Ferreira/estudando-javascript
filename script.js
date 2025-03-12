091a782a38c91acdb97896393a59ce3aba6bb52c
Rainer-Ferreira <rainerferreira.fisio@gmail.com>
2025-03-11 22:46:42 -0300

    Criando arrays no javascript

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 function adicionarTarefa () {
-    let mensagem= "Tarefa adicionada com sucesso!";
+
+    let tarefas = []
     
-    let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
-    document.getElementById("mensagem").textContent = mensagem
+    //recebe valor do input do usuario
+    const inputTarefa = document.getElementById("inputTarefa")
+    let tarefa = inputTarefa.value.trim()
+    
+    const mensagem = document.getElementById("mensagem")
 
-    let listaTarefas = document.getElementById("listaTarefas")
+ // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
+if (tarefa == "") {
+    //mostre uma mensagem de erro
+    let mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!"
+    mensagem.textContent = mensagemErro
+    } else {
+     //mensagem de tarefa adicionada com sucesso
+    let mensagemSucesso = "Tarefa adicionada com sucesso!";
+    mensagem.textContent = mensagemSucesso
+
+    //cria um novo item li e insere na lista ul
+    const listaTarefas = document.getElementById("listaTarefas")
     let novaTarefa = document.createElement("li")
-
     novaTarefa.textContent = tarefa 
-
     listaTarefas.appendChild(novaTarefa)
-
-    inputTarefa.value = ""
     }
+    
+
+    //limpa o input do usuário
+    inputTarefa.value = ""
+
+}
