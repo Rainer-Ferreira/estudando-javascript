25dd5d92cc12c39af4ec7ed84db82d4d8f2786aa
Rainer-Ferreira <rainerferreira.fisio@gmail.com>
2025-03-13 20:01:00 -0300

    Criando botões de Editar tarefa e Limpar

--- script.js
+++ script.js
@@ -36,10 +36,44 @@ function renderizarTarefas() {
 //3. se vai de 1 em 1 elemento ou se pula
 
     //for (iterador, condição, frequencia)
-    let i = 0
-    for (i; i < tarefas.length; i++) {
+    
+    for (let i = 0; i < tarefas.length; i++) {
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas[i]
+        
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+            
+            
+        novaTarefa.appendChild(botaoRemover)       
+        novaTarefa.appendChild(botaoEditar)      
         listaTarefas.appendChild(novaTarefa)
     }    
+}
+
+function removerTarefa (i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i) {
+    let tarefaEditada = prompt("Edite a tarefa:")
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    } 
+}
+
+function limparLista() {
+    tarefas.length = 0
+    renderizarTarefas()
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent = "Lista de Tarefas limpas com sucesso"
 }
